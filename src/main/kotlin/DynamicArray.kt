173c0938aae61146647a6af1fd572a514af119fe
danpgomez <dapgo3@gmail.com>
2021-04-26 17:18:14 -0400

    Adds implementation for remove and fixes push

--- src/main/kotlin/DynamicArray.kt
+++ src/main/kotlin/DynamicArray.kt
@@ -2,17 +2,24 @@ class DynamicArray<T>(
     private var values: MutableList<T>
 ) {
     var length: Int = values.size
+        private set
     var isEmpty: Boolean = length > 0
+        private set
 
     fun getValues() = values
     fun get(index: Int) = values[index]
 
     fun push(item: T): MutableList<T> {
-        values[length] = item
+        values.add(length, item)
         length++
         return values
     }
 
+    fun remove(item: T): MutableList<T> {
+        values.remove(item)
+        length--
+        return values
+    }
 }
 
 fun <T> mutableDynamicArray(vararg values: T): DynamicArray<T> {
@@ -22,7 +29,12 @@ fun <T> mutableDynamicArray(vararg values: T): DynamicArray<T> {
     }
     return DynamicArray(result)
 }
+
 fun main() {
     val myArray = mutableDynamicArray(1, 2, 3)
-    println(myArray.push(6))
+    myArray.push(6)
+    println(myArray.getValues())
+    println(myArray.isEmpty)
+    myArray.remove(3)
+    println(myArray.getValues())
 }
