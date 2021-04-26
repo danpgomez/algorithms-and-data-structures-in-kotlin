a77707304a137ef91537fcc19fed1b46cc0f7455
danpgomez <dapgo3@gmail.com>
2021-04-26 17:27:10 -0400

    Add time complexity annotations

--- src/main/kotlin/DynamicArray.kt
+++ src/main/kotlin/DynamicArray.kt
@@ -7,14 +7,18 @@ class DynamicArray<T>(
         private set
 
     fun getValues() = values
-    fun get(index: Int) = values[index]
 
+    // O(1) time complexity
+    fun lookup(index: Int) = values[index]
+
+    // O(1) time complexity
     fun push(item: T): MutableList<T> {
         values.add(length, item)
         length++
         return values
     }
 
+    // O(n) time complexity because we have to shift remaining items
     fun remove(item: T): MutableList<T> {
         values.remove(item)
         length--
@@ -33,6 +37,7 @@ fun <T> mutableDynamicArray(vararg values: T): DynamicArray<T> {
 fun main() {
     val myArray = mutableDynamicArray(1, 2, 3)
     myArray.push(6)
+    println(myArray.lookup(2))
     println(myArray.getValues())
     println(myArray.isEmpty)
     myArray.remove(3)
