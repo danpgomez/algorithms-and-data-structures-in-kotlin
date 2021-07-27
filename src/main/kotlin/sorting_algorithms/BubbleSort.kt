967684d6943f2c5a7f03ec7a5f30333cf2d16cb0
danpgomez <dapgo3@gmail.com>
2021-07-27 12:33:47 -0400

    Add implementation for BubbleSort.kt

---
+++ src/main/kotlin/sorting_algorithms/BubbleSort.kt
@@ -0,0 +1,22 @@
+package sorting_algorithms
+
+fun main() {
+    val numbers = arrayOf(99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0)
+
+    fun bubbleSort(array: Array<Int>): Array<Int> {
+        for (number in array) {
+            for (i in 0 until array.size - 1) {
+                if (array[i] > array[i + 1]) {
+                    val temp = array[i]
+                    array[i] = array[i + 1]
+                    array[i + 1] = temp
+                }
+            }
+        }
+        return array
+    }
+
+    bubbleSort(numbers).forEach {
+        println(it)
+    }
+}
