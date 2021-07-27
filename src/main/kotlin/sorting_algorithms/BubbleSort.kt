9c837733e8de02e8c2157dca6149e0dec5e21b53
danpgomez <dapgo3@gmail.com>
2021-07-27 12:36:07 -0400

    Add time complexity for BubbleSort.kt

--- src/main/kotlin/sorting_algorithms/BubbleSort.kt
+++ src/main/kotlin/sorting_algorithms/BubbleSort.kt
@@ -3,6 +3,7 @@ package sorting_algorithms
 fun main() {
     val numbers = arrayOf(99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0)
 
+    // O(n^2) time complexity since we have nested loops
     fun bubbleSort(array: Array<Int>): Array<Int> {
         for (number in array) {
             for (i in 0 until array.size - 1) {
